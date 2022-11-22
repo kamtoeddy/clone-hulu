9d049638143a2fc7f6d40a516687ea4f859334f8
kamtoeddy <lollipopkalog@gmail.com>
2022-11-22 19:37:27 +0100

    added loading bar for UX with SSR

--- components/Nav/index.jsx
+++ components/Nav/index.jsx
@@ -2,17 +2,28 @@ import { useRouter } from "next/router";
 
 import requests from "../../utils/requests";
 
+function setActiveClassName(key, { query }) {
+  const { genre } = query;
+
+  if (!genre) return "";
+
+  return genre === key ? "text-red-500" : "";
+}
+
 function Nav() {
   const router = useRouter();
 
   return (
     <nav className="relative">
       <div className="flex px-10 sm:px-20 text-2xl whitespace-nowrap space-x-10 sm:space-x-20 overflow-x-scroll scrollbar-hide">
-        {Object.entries(requests).map(([key, { title, usr }]) => (
+        {Object.entries(requests).map(([key, { title }]) => (
           <h2
             key={key}
             onClick={() => router.push(`/?genre=${key}`)}
-            className="last:pr-24 cursor-pointer transition duration transform hover:scale-125 hover:text-white active:text-red-500"
+            className={`last:pr-24 cursor-pointer transition duration transform hover:scale-125 hover:text-white active:text-red-500 ${setActiveClassName(
+              key,
+              router
+            )}`}
           >
             {title}
           </h2>
--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,15 @@
-import '../styles/globals.css'
+// components
+import NextNProgress from "nextjs-progressbar";
+
+import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <NextNProgress color="#ef4444" options={{ showSpinner: false }} />
+      <Component {...pageProps} />
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
